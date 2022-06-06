2b9e28eed65006caea8854d34e7eea4411ba68c7
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-06 22:13:52 +0430

    add get

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -3,7 +3,10 @@ const User = require("../models/users.model");
 
 const postUser = asyncWrapper(async (req, res) => {});
 
-const getAllUsers = asyncWrapper(async (req, res) => {});
+const getAllUsers = asyncWrapper(async (req, res) => {
+    const getUsers = await User.find({});
+    res.status(200).json(getUsers)
+});
 
 const getOneUser = asyncWrapper(async (req, res) => {});
 
